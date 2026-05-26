510dfcb4ae584dbb6590a0d19523d00bbd797064
InvisibleFunction <3682184+InvisibleFunction@users.noreply.github.com>
2026-05-25 23:26:55 -0400

    Update init

--- beetsplug/__init__.py
+++ beetsplug/__init__.py
@@ -1,2 +1,2 @@
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
